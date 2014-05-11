fc78f30798fff06e154dbbb2c4814039a9e24f9b
Akira Higuchi <ahiguti100@gmail.com>
2014-05-12 05:04:13 +0900

    cleanups

--- pxc/misc/android-ndk/sample/apply_sdl_diff.sh
+++ pxc/misc/android-ndk/sample/apply_sdl_diff.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+cd `dirname $0`
 # remove harmful System.loadLibrary() calls
 cat ./jni/SDL2/android-project/src/org/libsdl/app/SDLActivity.java \
   | sed -e 's/System\.loadLibrary.*/\/\//g' \
--- pxc/misc/android-ndk/sample/build_debug.sh
+++ pxc/misc/android-ndk/sample/build_debug.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+cd `dirname $0`
 ./apply_sdl_diff.sh
 pushd ./jni/pxsrc/ && ./pxc2cc.sh && popd
 ndk-build NDK_DEBUG=1
