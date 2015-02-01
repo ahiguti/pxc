9aa694489f2a678064d4dfc454e5b1af593e6593
Akira Higuchi <ahiguti100@gmail.com>
2015-02-02 01:03:48 +0900

    statically-typed user-defined field operator

--- pxc/tests/skip_137_python_timing/timing.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# env pxc_timing_param=1000000 ./run.sh pytiming1.px 
-env pxc_timing_param=1000 ./run.sh pytiming2.px 
-# env pxc_timing_param=10000000 ./run.sh pytiming3.px 
-# env pxc_timing_param=100000 ./run.sh pytiming4.px 
-# env pxc_timing_param=10000000 ./run.sh pytiming5.px 
-
