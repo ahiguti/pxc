5208af13536ca25eca852f039d5b5f6e0ac8ec29
Akira Higuchi <364452+ahiguti@users.noreply.github.com>
2016-10-11 00:47:35 +0900

    pgl3d: edit_mode

--- pxc/misc/pgl3d/build_exec.sh
+++ pxc/misc/pgl3d/build_exec.sh
@@ -3,8 +3,10 @@
 cd `dirname $0`
 if [ "`uname | cut -d '_' -f 1`" == "CYGWIN" ]; then
 	./windows/release_build.sh && \
-		exec ./windows/x64/Release/pgl3d_demoapp.exe 2>&1 | tee /tmp/z
+	  exec ./windows/x64/Release/pgl3d_demoapp.exe $* 2>&1 | \
+	  tee /tmp/pgl3d.log
 else
 	./unix/release_build.sh && \
-		exec ./demoapp.px.exe 2>&1 | tee /tmp/z
+	  exec ./demoapp.px.exe 2>&1 | \
+	  tee /tmp/pgl3d.log
 fi
